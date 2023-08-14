b7c167eb456f0d55bbffe0c0b4c040891ae79aa5
Luiz Henrique <65136349+luizhsousa98@users.noreply.github.com>
2023-08-13 21:03:46 -0300

    Projeto Integrado Prototipo

---
+++ js/script.js
@@ -0,0 +1,75 @@
+function renderizarParaTelaDeRastreio() {
+    location.href = 'rastreioDePedido.html';
+}
+
+function renderizarParaLoginAdministrador() {
+    location.href = 'loginAdministrador.html';
+}
+
+function renderizarParaLoginGestor() {
+    location.href = 'loginGestor.html';
+}
+function renderizarParaLoginMotorista() {
+    location.href = 'loginMotorista.html';
+}
+
+function renderizarParaListarHistoricoViagens() {
+    location.href = 'gerenciarHistoricoViagem.html';
+}
+
+function renderizarParaCadastrarHistoricoViagens() {
+    location.href = 'cadastrarHistoricoViagem.html';
+}
+
+function renderizarParaListarViagens() {
+    location.href = 'gerenciamentoViagens.html';
+}
+
+
+function renderizarParaPortalDoColaborador(){
+    location.href = 'portalColaboradorAcess.html';
+}
+
+function renderizarParaAgendametoViagens() {
+    location.href = 'cadastrarViagem.html';
+}
+function renderizarParaEditarAgendametoViagens() {
+    location.href = 'editarViagem.html';
+}
+
+function renderizarParaGerenciamentoUsuario(){
+    location.href = 'gerenciarUsuario.html';
+}
+
+function renderizarParaEditarUsuario(){
+    location.href = 'editarUsuario.html';
+}
+
+function renderizarParaCadastroUsuario(){
+    location.href = 'cadastrarUsuario.html';
+}
+
+///////////////Gestor////////////////////////////////////////////////////////////
+function renderizarParaAgendametoViagensGestor() {
+    location.href = 'cadastrarViagemGestor.html';
+}
+function renderizarParaEditarAgendametoViagensGestor() {
+    location.href = 'editarViagemGestor.html';
+}
+
+function renderizarParaListarHistoricoViagensGestor() {
+    location.href = 'gerenciarHistoricoViagemGestor.html';
+}
+
+/////////////////////////////Motorista/////////////////////////////////////
+function renderizarParaAgendametoViagensMotorista() {
+    location.href = 'cadastrarViagemMotorista.html';
+}
+
+function renderizarParaGerenciarHistoricoViagensMostorista() {
+    location.href = 'gerenciarHistoricoViagemMotorista.html';
+}
+
+function renderizarParaCadastrarHistoricoMostorista() {
+    location.href = 'cadastrarHistoricoViagemMotorista.html';
+}
